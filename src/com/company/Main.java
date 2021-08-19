3c11dc18e76da8bbe0cd98efe659754b86ad68c9
farshad <farshadnp6@gmail.com>
2021-08-20 01:31:20 +0430

    initilizeing

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.flush();
+        String[] Names={"Farshad","Reza"};
+        for(int i=0;i<Names.length;i++)
+        {
+            System.out.println(Names[i]);
+        }
+
+    }
+}
